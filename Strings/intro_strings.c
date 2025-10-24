d5348b1fee986c47dd04bc4c141672fe20146fb8
madhavzanwar <madhavzanwar13@gmail.com>
2025-10-25 02:23:40 +0530

    modified program

--- Strings/intro_strings.c
+++ Strings/intro_strings.c
@@ -4,6 +4,8 @@ int main() {
     //strings are arrays of characters ending with a null character '\0'
 
 char st[] = { 'a', 'b', 'c', 'd', '\0' }; //declaration of a string
+
+// char st[] = "abcd"; //another way to declare a string
 // '\0' is used to indicate the end of the string, its called a null character
 
 printf("First character: %c\n", st[0]);
@@ -12,5 +14,7 @@ printf("Third character: %c\n", st[2]);
 printf("Fourth character: %c\n", st[3]);
 printf("Fifth character (null terminator): %c\n", st[4]);
 
+//printf("%s", st); //to print the whole string its a lot more convenient
+//here we will not use & operator because st is already a pointer to the first character of the string
  return 0;
 }
