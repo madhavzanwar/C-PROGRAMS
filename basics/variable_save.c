8ca5babad05bf60687236919db33f1d5e257090c
Madhav <madhavzanwar13@gmail.com>
2025-11-26 10:13:55 +0530

    Clean up variable_save.c by removing unused code
    Removed old variable initialization and print statements.

--- basics/variable_save.c
+++ basics/variable_save.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int main() 
@@ -14,20 +13,3 @@ int main()
     
     
 }
-=======
-#include <stdio.h>
-
-int main() 
-{
-    int a,b;
-    a = 6;
-    b = 4;
-    
-    printf("%d",a);
-
-
-    return 0;
-    
-    
-}
->>>>>>> 41fb830a2cada7ee8388ad479b2c96bcad7a3379
